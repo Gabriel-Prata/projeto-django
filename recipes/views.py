86e77a8248c87e14120d67bb0cbc191f55ffbd04
Gabriel-Prata <gabrielreisnp@gmail.com>
2022-08-02 17:48:47 -0300

    factory

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,17 @@
 import datetime
 from django.shortcuts import render
+from utility.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'GABRIEL',
+        'recipes': [make_recipe() for _ in range(10)],
+        'name': 'Gabriel Prata',
         'data': datetime.datetime.now()
         })
+
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
