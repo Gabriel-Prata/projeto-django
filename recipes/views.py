4f3c488dee790929625dd1175e95e9ca8540b2d2
Gabriel-Prata <gabrielreisnp@gmail.com>
2022-08-03 18:37:21 -0300

    base_de_dados

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,50 @@
 import datetime
+from django.http import Http404
 from django.shortcuts import render
-from utility.factory import make_recipe
+from .models import Recipe
+
+
+name = 'Gabriel Prata'
+date = datetime.datetime.now()
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
-        'name': 'Gabriel Prata',
-        'data': datetime.datetime.now()
+        'recipes': recipes,
+        'name': name,
+        'data': date,
         })
 
 
-def recipes(request, id):
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+        ).order_by('id')
+
+    if not recipes:
+        raise Http404('NONECXISTE')
+
+    category_name = recipes.first().category.name
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'category_name': category_name,
+        'name': name,
+        'data': date,
+        })
+
+
+
+
+def recipes(request, recipe_id):
+    recipe = Recipe.objects.filter(
+        id=recipe_id,
+        is_published=True,
+    ).first()
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
+        'name': name,
+        'data': date,
     })
